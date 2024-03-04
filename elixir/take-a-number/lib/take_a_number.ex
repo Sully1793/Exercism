6a29e9451994f80da0ed0f4fc1b02a603558fae4
Sully1793 <sulaiman.cassim17@gmail.com>
2024-03-04 15:43:43 +1100

    new exercrise added

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,23 @@
 defmodule TakeANumber do
   def start() do
     # Please implement the start/0 function
+  spawn(fn -> loop(0)  end)
+  end
+
+  defp loop(number) do
+    receive do
+      {:report_state, sender} ->
+        send(sender, number)
+        loop(number)
+
+      {:take_a_number, sender} ->
+        send(sender, number + 1)
+        loop(number + 1)
+
+        :stop -> nil
+
+        _ -> loop (number)
+    end
+
   end
 end
